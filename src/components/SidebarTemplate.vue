1db03a65a70b04a2ee6b97f2466d63862005888c
Hyrum Cooper <hyrumcoop@gmail.com>
2021-01-08 12:21:21 -0500

    created SidebarTemplate component w/ slot in body

---
+++ src/components/SidebarTemplate.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class='container-fluid h-100'>
+    <div class='row h-100'>
+      <div class='col-auto p-0'>
+        <sidebar :links='links' />
+      </div>
+      <div class='col'>
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from '@/components/Sidebar'
+
+export default {
+  name: 'SidebarTemplate',
+  components: {
+    Sidebar
+  },
+  props: ['links']
+}
+</script>
