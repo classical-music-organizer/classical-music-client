21620ed61f3bdcc26f340c896dce1c8a1c889dd8
Hyrum Cooper <hyrumcoop@gmail.com>
2021-01-08 12:30:47 -0500

    added composer page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import Composer from '@/views/Composer'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/about',
     name: 'about',
     component: About
+  },
+  {
+    path: '/composer/:id',
+    name: 'composer',
+    component: Composer
   }
 ]
 
---
+++ src/views/Composer.vue
@@ -0,0 +1,48 @@
+<template>
+  <sidebar-template :links='sidebarLinks'>
+    <h1>{{ composer.fullName }}</h1>
+    <p>{{ composer.info }}</p>
+  </sidebar-template>
+</template>
+
+<script>
+import SidebarTemplate from '@/components/SidebarTemplate'
+
+export default {
+  name: 'Composer',
+  components: {
+    SidebarTemplate
+  },
+  computed: {
+    composer() {
+      // TODO: return real composer
+      return {fullName: 'Johann Sebastian Bach', info: 'Bach was a great baroque composer.'}
+    },
+    sidebarLinks() {
+      // TODO: return real tag links
+      return [
+        {
+          heading: {text: 'Baroque Composers', link: '/tag/1'},
+          links: [
+            {text: 'Johann Sebastian Bach', link: '/composer/1'},
+            {text: 'Antonio Vivaldi', link: '/composer/2'},
+            {text: 'George Frideric Handel', link: '/composer/3'},
+            {text: 'Claudio Monteverdi', link: '/composer/4'},
+            {text: 'Georg Philipp Telemann', link: '/composer/5'}
+          ]
+        },
+        {
+          heading: {text: 'Accessible Composers', link: '/'},
+          links: [
+            {text: 'Johann Sebastian Bach', link: '/composer/1'},
+            {text: 'Antonio Vivaldi', link: '/composer/2'},
+            {text: 'George Frideric Handel', link: '/composer/3'},
+            {text: 'Claudio Monteverdi', link: '/composer/4'},
+            {text: 'Georg Philipp Telemann', link: '/composer/5'}
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
