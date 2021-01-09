f2073fbc5affeaea039a5283e4a93c0db5825472
Hyrum Cooper <hyrumcoop@gmail.com>
2021-01-08 21:20:21 -0500

    created tag-link component

---
+++ src/components/TagLink.vue
@@ -0,0 +1,38 @@
+<template>
+  <span class='tag'>
+    <router-link to='/tag/1'>
+      <slot></slot>
+    </router-link>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'TagLink',
+  props: ['link']
+}
+</script>
+
+<style scoped>
+
+.tag {
+  margin-right: 8px;
+  padding: 6px 14px;
+  background-color: #fff176;
+  border-radius: 6px;
+
+  font-weight: 500;
+
+  transition: background-color .1s ease;
+}
+
+.tag a {
+  text-decoration: none;
+  color: #212121;
+}
+
+.tag:hover {
+  background-color: #fdd835;
+}
+
+</style>
