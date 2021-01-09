cccc5263a2692d743775065f79bc08b00fc4b4a1
Hyrum Cooper <hyrumcoop@gmail.com>
2021-01-08 20:28:05 -0500

    load composer on composer page

--- src/views/Composer.vue
+++ src/views/Composer.vue
@@ -1,11 +1,12 @@
 <template>
   <sidebar-template :links='sidebarLinks'>
-    <h1>{{ composer.fullName }}</h1>
-    <p>{{ composer.info }}</p>
+    <h2>{{ loaded ? composer.fullName : 'Loading' }}</h2>
+    <p>{{ loaded ? composer.info.content : 'Loading' }}</p>
   </sidebar-template>
 </template>
 
 <script>
+import Api from '@/api'
 import SidebarTemplate from '@/components/SidebarTemplate'
 
 export default {
@@ -13,11 +14,46 @@ export default {
   components: {
     SidebarTemplate
   },
+  data() {
+    return {
+      loaded: false,
+      composer: null
+    }
+  },
+  async mounted() {
+    const { id } = this.$route.params
+
+    await this.retrieveComposer(id)
+  },
+  async beforeRouteUpdate(to) {
+    // TODO: when redirecting to slugified route, use `verifiedSlug` in route metadata to avoid reloading component
+
+    if (to.name == 'composer' || to.name == 'composerSlug') {
+      // TODO: at some point when updating, we need to make sure slug still equals composer's slug?
+      this.retrieveComposer(to.params.id)
+      
+    }
+  },
+  methods: {
+    // retrieves the composer object and sets loaded to true
+    async retrieveComposer(id) {
+      this.loaded = false
+      this.composer = null
+ 
+      try {
+        this.composer = await Api.composer.retrieve(id)
+      } catch(err) {
+        if (err.code == 404) { // TODO: create strongly typed errors (NotFoundError) and use instanceof
+          // TODO: render not found
+        } else {
+          throw err
+        }
+      } finally {
+        this.loaded = true
+      }
+    }
+  },
   computed: {
-    composer() {
-      // TODO: return real composer
-      return {fullName: 'Johann Sebastian Bach', info: 'Bach was a great baroque composer.'}
-    },
     sidebarLinks() {
       // TODO: return real tag links
       return [
